246af4a0a8f2e5e4770f59fb03acc46176ef1152
Pattarasak Raksanarong <rogerlovek@gmail.com>
2025-03-12 19:00:51 +0700

    finish assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,16 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let surname = "Anderson";
+let restaurantAddress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const TotalFoodPrice = 15000;
+const totalOpenRestaurant = 1500;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+restaurantAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+TotalFoodPrice = 5000;
+totalOpenRestaurant = 2500;
